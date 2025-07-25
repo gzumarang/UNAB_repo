013b2ffa8e0666bae573c50489ba41aaf9d604cb
GZumaran <gzumaran@gmail.com>
2025-07-25 19:12:08 -0400

    Add files via upload

---
+++ Eje_2.py
@@ -0,0 +1,16 @@
+print("¡¡¡hola mundo!!!")
+print("sin traductor")
+# Se utiliza para comentar una línea
+print("esta línea está después de un comentario con #")
+
+""" Se utiliza para comentar varias líneas
+Este es python y resulta interesante
+"""
+print("esta línea está después de un comentario con 3 comillas ")
+
+valor1 = 10
+print(valor1)
+valor2 = 15
+print(valor2)
+valor1 = valor2 + 10 + valor1
+print(valor1)
---
+++ Eje_3.py
@@ -0,0 +1,5 @@
+a = int(input("Ingrese número 1:"))
+b = int(input("ingrese número 2:"))
+resultado = a * b
+print(f"resultado es: {resultado}")
+resultado
